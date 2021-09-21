7acf4c66964c26748cae17ec4d67069b6e895367
Viktor Pyatakov <pyatakov.viktor@gmail.com>
2021-09-21 13:24:02 +0300

    change deploy

--- clients/admin-ui/src/configs/routes.js
+++ clients/admin-ui/src/configs/routes.js
@@ -1,37 +1,37 @@
 
 const routes = [
-  {
-    path: '/',
-    wrapper: 'Navigate',
-    module: import(
-      /* webpackChunkName: "orders" */
-      '@modules/order-main'
-    ),
-  },
-  {
-    path: '/customers',
-    wrapper: 'Navigate',
-    module: import(
-      /* webpackChunkName: "customers" */
-      '@modules/order-main'
-    ),
-  },
-  {
-    path: '/sign-in',
-    wrapper: 'Empty',
-    module: import(
-      /* webpackChunkName: "sign-in" */
-      '@modules/order-sign-in'
-    ),
-  },
-  {
-    path: '/sign-up',
-    wrapper: 'Empty',
-    module: import(
-      /* webpackChunkName: "sign-up" */
-      '@modules/order-sign-up'
-    ),
-  },
+  // {
+  //   path: '/',
+  //   wrapper: 'Navigate',
+  //   module: import(
+  //     /* webpackChunkName: "orders" */
+  //     '@modules/order-main'
+  //   ),
+  // },
+  // {
+  //   path: '/customers',
+  //   wrapper: 'Navigate',
+  //   module: import(
+  //     /* webpackChunkName: "customers" */
+  //     '@modules/order-main'
+  //   ),
+  // },
+  // {
+  //   path: '/sign-in',
+  //   wrapper: 'Empty',
+  //   module: import(
+  //     /* webpackChunkName: "sign-in" */
+  //     '@modules/order-sign-in'
+  //   ),
+  // },
+  // {
+  //   path: '/sign-up',
+  //   wrapper: 'Empty',
+  //   module: import(
+  //     /* webpackChunkName: "sign-up" */
+  //     '@modules/order-sign-up'
+  //   ),
+  // },
 ];
 
 export default routes;
