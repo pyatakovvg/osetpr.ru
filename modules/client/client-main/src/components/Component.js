5cfa03022d13be2a42f27793f307ec721c2d5732
Viktor Pyatakov <pyatakov.viktor@gmail.com>
2021-10-23 18:10:58 +0300

    client page

--- modules/client/client-main/src/components/Component.js
+++ modules/client/client-main/src/components/Component.js
@@ -1,7 +1,7 @@
 
-import { Header, Page, PageContent } from '@ui.packages/admin-kit';
-
 import React from 'react';
+
+import logotype from './Logotype.svg';
 
 import styles from './default.module.scss';
 
@@ -10,7 +10,12 @@ function Main() {
   return (
     <section className={styles['wrapper']}>
       <div className={styles['content']}>
-
+        <img className={styles['logotype']} src={logotype} width={200} height={200} alt={''} />
+        <div className={styles['description']}>
+          <p className={styles['text']}>Магазин находится в разработке</p>
+          <p className={styles['text-2']}>По вопросам сотрудничества, заказов и доставки, обращаться по номеру телефона</p>
+          <p className={styles['phone']}>+ 7 (978) 590-60-60</p>
+        </div>
       </div>
     </section>
   );
