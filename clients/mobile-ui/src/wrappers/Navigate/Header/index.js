69ee97a38a266857c088bf6abab5f897e2e8801b
Viktor Pyatakov <pyatakov.viktor@gmail.com>
2021-10-28 17:29:10 +0300

    mobile version

--- clients/mobile-ui/src/wrappers/Navigate/Header/index.js
+++ clients/mobile-ui/src/wrappers/Navigate/Header/index.js
@@ -2,6 +2,7 @@
 import { Logotype } from '@ui.packages/mobile-kit';
 
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import styles from './default.module.scss';
 
@@ -10,7 +11,9 @@ export default function Header() {
   return (
     <div className={styles['wrapper']}>
       <div className={styles['logotype']}>
-        <Logotype />
+        <Link to={process.env['PUBLIC_URL'] + '/'}>
+          <Logotype />
+        </Link>
       </div>
     </div>
   );
