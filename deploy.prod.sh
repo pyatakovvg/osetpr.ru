97ca2c12de104efac8a97a94e9621a52f0270c15
Viktor Pyatakov <pyatakov.viktor@gmail.com>
2021-11-03 23:24:20 +0300

    proxy nginx

--- deploy.prod.sh
+++ deploy.prod.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-docker-compose up --build -d
+docker-compose up --build -d --file compose.prod.yml
 
 # shellcheck disable=SC2046
 docker rmi $(docker images -f dangling=true -q) --force
