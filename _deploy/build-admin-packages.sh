f4abee2577876ac02d496ab2e37e509aaedd2a13
Viktor Pyatakov <pyatakov.viktor@gmail.com>
2021-11-04 01:34:21 +0300

    deploy

--- _deploy/build-admin-packages.sh
+++ _deploy/build-admin-packages.sh
@@ -4,7 +4,7 @@ echo ''
 echo 'Сборка пакетов "UI" для "Admin"'
 echo '-------------------'
 
-cd ../ui.packages || return
+cd ./ui.packages || return
 
 echo '[--- Application ---]'
 cd  ./application && npx yarn build
--- _deploy/build-client-packages.sh
+++ _deploy/build-client-packages.sh
@@ -4,7 +4,7 @@ echo ''
 echo 'Сборка пакетов "UI" для "Order"'
 echo '-------------------'
 
-cd ../ui.packages || return
+cd ./ui.packages || return
 
 echo '[--- Application ---]'
 cd  ./application && npx yarn build
