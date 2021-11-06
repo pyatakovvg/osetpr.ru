4683021191d00b6710eef2a3c4ee394433ee74d1
Viktor Pyatakov <pyatakov.viktor@gmail.com>
2021-11-06 15:48:23 +0300

    product category order

--- services/product-srv/src/controllers/category/create/index.mjs
+++ services/product-srv/src/controllers/category/create/index.mjs
@@ -1,30 +1,7 @@
 
-import { models } from '@sys.packages/db';
-import { sendEvent } from "@sys.packages/rabbit";
-
-
 export default () => async (ctx) => {
-  const { Product } = models;
-  const formData = ctx['request']['body'];
-
-  const { uuid } = await Order.create(formData);
-
-  const result = await Product.findOne({
-    where: { uuid },
-    attributes: ['uuid', 'userUuid', 'title', 'description', 'dateTo', 'address', 'createdAt', 'updatedAt'],
-    include: [
-      {
-        model: Status,
-        required: true,
-        as: 'status',
-      },
-    ],
-  });
-
-  await sendEvent(process.env['EXCHANGE_UNIT_CREATE'], JSON.stringify(result.toJSON()));
-
   ctx.body = {
     success: true,
-    data: result.toJSON(),
+    data: null,
   };
 };
