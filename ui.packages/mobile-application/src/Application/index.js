f4787e1fa37742bc19f8b9a70a3b919b2bf501c6
Viktor Pyatakov <pyatakov.viktor@gmail.com>
2021-11-08 01:51:56 +0300

    push notifications

--- ui.packages/mobile-application/src/Application/index.js
+++ ui.packages/mobile-application/src/Application/index.js
@@ -1,9 +1,8 @@
 
 import { on, off } from '@ui.packages/socket';
-import { checkSubscription, subscribeUser } from '@ui.packages/web-push';
-import { Confirm, openDialog, closeDialog } from '@ui.packages/mobile-dialog';
+import { checkSubscription, subscribeUser, checkServiceWorker } from '@ui.packages/web-push';
 
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 
 import Router from '../Router';
@@ -21,8 +20,6 @@ function Application({ options }) {
   const dispatch = useDispatch();
   const isLoaded = useSelector(selectIsLoaded);
 
-  const [isPushNotifications, setPushNotifications] = useState(true);
-
   if (options['useSignIn']) {
     useRedirect();
     useGetProfile();
@@ -31,31 +28,24 @@ function Application({ options }) {
     dispatch(isLoadedAction());
   }
 
-  if (options['useSignIn']) {
-    useEffect(function () {
+  useEffect(async () => {
+    if (checkServiceWorker()) {
+      if ( ! await checkSubscription()) {
+        await subscribeUser(window.localStorage.getItem('userUuid'));
+      }
+    }
+  }, []);
+
+  useEffect(function () {
+    if (options['useSignIn']) {
       on(process.env['REACT_APP_SOCKET_CUSTOMER_UPDATE'], function(data) {
         dispatch(updateCustomerAction(data));
       });
       return () => {
         off(process.env['REACT_APP_SOCKET_CUSTOMER_UPDATE']);
       };
-    }, []);
-  }
-
-  useEffect(async () => {
-    setPushNotifications(await checkSubscription());
-  }, []);
-
-  useEffect(() => {
-    if ( ! isPushNotifications) {
-      dispatch(openDialog('push-subscribe'));
     }
-  }, [isPushNotifications]);
-
-  async function handleSubscribe() {
-    await subscribeUser(window.localStorage.getItem('userUuid'));
-    dispatch(closeDialog());
-  }
+  }, []);
 
   return (
     <section className={styles['application']}>
@@ -64,13 +54,6 @@ function Application({ options }) {
           ? <Router />
           : <Loading />}
       </ApplicationContext.Provider>
-      { ! isPushNotifications && (
-        <Confirm
-          name={'push-subscribe'}
-          message={'Вы хотели бы получать информацию о статусе заказа посредством пуш-уведомлений?'}
-          onApply={() => handleSubscribe()}
-        />
-      )}
     </section>
   );
 }
