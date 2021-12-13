bf9d45a8551ac17f46571203fbe4e3bc57756f3f
Viktor Pyatakov <pyatakov.viktor@gmail.com>
2021-12-13 12:40:42 +0300

    фильтер стате

--- modules/client/main/src/components/Filter/Category/index.js
+++ modules/client/main/src/components/Filter/Category/index.js
@@ -1,16 +1,44 @@
 
 import { selectFilter } from '@modules/client-main';
 
+import { queryToObject } from "@ui.packages/utils";
 import { Header, Text, Checkbox } from '@ui.packages/client-kit';
 
-import React from 'react';
 import { useSelector } from 'react-redux';
+import { useLocation } from "react-router-dom";
+import React, { useState, useMemo, useEffect } from 'react';
 
+import cn from 'classnames';
 import styles from './default.module.scss';
 
 
+function useQuery(name) {
+  const location = useLocation();
+  const [hasItems, setHasItems] = useState(false);
+  useEffect(() => {
+    const query = queryToObject(location['search']);
+    setHasItems(name in query);
+  }, [location['search']]);
+  return hasItems;
+}
+
 function Groups({ values, onChange }) {
+  const [isOpen, setOpen] = useState(true);
+
   const filter = useSelector(selectFilter);
+  const hasSelected = useQuery('categoryUuid');
+
+  const wrapperClassName = useMemo(() => cn(styles['wrapper'], {
+    [styles['is-has']]: hasSelected,
+  }), [hasSelected]);
+  const iconClassName = useMemo(() => cn(styles['icon'], {
+    ['fas fa-minus']: isOpen,
+    ['fas fa-plus']: ! isOpen,
+  }), [isOpen]);
+
+  function handleCollapse() {
+    setOpen( ! isOpen);
+  }
 
   function handleChange(value) {
     let newValues;
@@ -24,19 +52,22 @@ function Groups({ values, onChange }) {
   }
 
   return (
-    <div className={styles['wrapper']}>
-      <div className={styles['header']}>
-        <Header level={3}>Категория</Header>
+    <div className={wrapperClassName}>
+      <div className={styles['header']} onClick={handleCollapse}>
+        <Header level={3} className={styles['title']}>Категория</Header>
+        <span className={iconClassName} />
       </div>
-      <div className={styles['content']}>
-        {(filter['categories'] || []).map((item) => (
-          <div key={item['uuid']} className={styles['checkbox']}>
-            <Checkbox value={ !!~ values.indexOf(item['uuid'])} onChange={() => handleChange(item['uuid'])}>
-              <Text>{ item['value'] }</Text>
-            </Checkbox>
-          </div>
-        ))}
-      </div>
+      {isOpen && (
+        <div className={styles['content']}>
+          {(filter['categories'] || []).map((item) => (
+            <div key={item['uuid']} className={styles['checkbox']}>
+              <Checkbox value={ !!~ values.indexOf(item['uuid'])} onChange={() => handleChange(item['uuid'])}>
+                <Text>{ item['value'] }</Text>
+              </Checkbox>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
--- modules/client/main/src/components/Filter/Groups/index.js
+++ modules/client/main/src/components/Filter/Groups/index.js
@@ -1,20 +1,49 @@
 
 import { selectFilter } from '@modules/client-main';
 
+import { queryToObject } from '@ui.packages/utils';
 import { Header, Text, Checkbox } from '@ui.packages/client-kit';
 
-import React from 'react';
 import { useSelector } from 'react-redux';
+import { useLocation } from 'react-router-dom';
+import React, { useState, useMemo, useEffect } from 'react';
 
+import cn from 'classnames';
 import styles from './default.module.scss';
 
 
+function useQuery(name) {
+  const location = useLocation();
+  const [hasItems, setHasItems] = useState(false);
+  useEffect(() => {
+    const query = queryToObject(location['search']);
+    setHasItems(name in query);
+  }, [location['search']]);
+  return hasItems;
+}
+
+
 function Groups({ values, onChange }) {
+  const [isOpen, setOpen] = useState(true);
+
   const filter = useSelector(selectFilter);
+  const hasSelected = useQuery('groupUuid');
+
+  const wrapperClassName = useMemo(() => cn(styles['wrapper'], {
+    [styles['is-has']]: hasSelected,
+  }), [hasSelected]);
+  const iconClassName = useMemo(() => cn(styles['icon'], {
+    ['fas fa-minus']: isOpen,
+    ['fas fa-plus']: ! isOpen,
+  }), [isOpen]);
+
+  function handleCollapse() {
+    setOpen( ! isOpen);
+  }
 
   function handleChange(value) {
     let newValues;
-    if (!!~ values.indexOf(value)) {
+    if ( !!~ values.indexOf(value)) {
       newValues = values.filter((id) => id !== value);
     }
     else {
@@ -24,19 +53,22 @@ function Groups({ values, onChange }) {
   }
 
   return (
-    <div className={styles['wrapper']}>
-      <div className={styles['header']}>
-        <Header level={3}>Группа</Header>
+    <div className={wrapperClassName}>
+      <div className={styles['header']} onClick={handleCollapse}>
+        <Header level={3} className={styles['title']}>Группа</Header>
+        <span className={iconClassName} />
       </div>
-      <div className={styles['content']}>
-        {(filter['groups'] || []).map((item) => (
-          <div key={item['uuid']} className={styles['checkbox']}>
-            <Checkbox value={ !!~ values.indexOf(item['uuid'])} onChange={() => handleChange(item['uuid'])}>
-              <Text>{ item['value'] }</Text>
-            </Checkbox>
-          </div>
-        ))}
-      </div>
+      {isOpen && (
+        <div className={styles['content']}>
+          {(filter['groups'] || []).map((item) => (
+            <div key={item['uuid']} className={styles['checkbox']}>
+              <Checkbox value={ !!~ values.indexOf(item['uuid'])} onChange={() => handleChange(item['uuid'])}>
+                <Text>{ item['value'] }</Text>
+              </Checkbox>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
