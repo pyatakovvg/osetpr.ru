29cc47ab5f4ec272dd7f08b1876da360587b4695
Viktor Pyatakov <pyatakov.viktor@gmail.com>
2021-12-14 20:56:01 +0300

    admin logotype

--- clients/admin-ui/src/wrappers/Navigate/Header/index.js
+++ clients/admin-ui/src/wrappers/Navigate/Header/index.js
@@ -1,4 +1,6 @@
 
+import { Logotype } from '@ui.packages/admin-kit';
+
 import React from 'react';
 import { Link } from 'react-router-dom';
 
@@ -10,10 +12,10 @@ import styles from './default.module.scss';
 export default function Header() {
   return (
     <div className={styles['header']}>
-      <div className={styles['header__title']}>
-        <div className={styles['container']}>
-          <Link className={styles['logotype']} to={'/'}><i className="fas fa-pizza-slice" />&nbsp;&nbsp;&nbsp;Осетинские пироги</Link>
-        </div>
+      <div className={styles['content']}>
+        <Link className={styles['logotype']} to={process.env['PUBLIC_URL'] + '/'}>
+          <Logotype />
+        </Link>
       </div>
       <div className={styles['controls']}>
         <Profile />
