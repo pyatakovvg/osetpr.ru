a7af78e1431f055d9b51388ff62f05adbf7c6e02
Viktor Pyatakov <pyatakov.viktor@gmail.com>
2021-12-14 13:12:38 +0300

    --remove-orphans

--- deploy.stage.sh
+++ deploy.stage.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-docker-compose --file ./compose.stage.yml up --build -d
+docker-compose --file ./compose.stage.yml up --build -d --remove-orphans
 
 # shellcheck disable=SC2046
 docker rmi $(docker images -f dangling=true -q) --force
